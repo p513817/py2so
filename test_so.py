e88c87451270410b3145f4562bf3bd47e004d51b
MaxChangInnodisk <max_chang@innodisk.com>
2022-03-08 17:37:54 +0800

    feat: test the python api with .so is available.

---
+++ test_so.py
@@ -0,0 +1,7 @@
+import out.foo.print_me as foo_print_me
+import out.bar.print_me as bar_print_me
+import out.bar.barbar.print_me as barbar_print_me
+
+foo_print_me.do()
+bar_print_me.do()
+barbar_print_me.do()
