3ccc7f369735dc155a12111fd77b81aafa5066dc
MaxChangInnodisk <max_chang@innodisk.com>
2022-03-09 10:56:50 +0800

    fix: provide a temporary destination since the setup.py will package the .so file in the same path.

--- setup.py
+++ setup.py
@@ -9,27 +9,28 @@ except:
     print("copy from www.cython.org and install it")
     sys.exit(1)
 
-def get_arguments(args:list, key:str):
+# get custom argument
+def get_args(args:list, key:str):
     if key in args:
         idx = args.index(key)
         try:
             val = args[int(idx)+1]           # get value of argument
             [ sys.argv.remove(i) for i in [key, val] ] # remove argument or setup will get error
-            return val
+            return os.path.abspath(val)
         except Exception as e:
             return None
 
 args = sys.argv
-build_path = "build"
 
 # check basic argument
 for opt in ['build_ext', '--inplace']:
     if not (opt in args):
         raise Exception('\nExcepted command is $ python3 setup.py build_ext --inplace <--src> <src path> <--dst> <dst path>') 
 
-# get custom option
-src, dst = "src", "dst"
-src, dst = [ get_arguments(args, opt) for opt in ['--src', '--dst'] ]
+# parse custom option
+src, dst = [ get_args(args, opt) if get_args(args, opt)!=None else opt.replace('--','') for opt in ['--src', '--dst'] ]
+temp_dst = os.path.join( os.getcwd(), os.path.basename(dst) )
+build_path = './build'
 
 # check the source is exists
 if not os.path.exists(src):
@@ -37,14 +38,14 @@ if not os.path.exists(src):
 
 # copy the structure and rename and clear pycache
 [ shutil.rmtree(f) for f in glob.glob(f"{src}/**/__pycache__", recursive=True) ]
-shutil.copytree(src, dst)
+shutil.copytree(src, temp_dst)
 
 # cpature all python files but exclude __init__.py
-extensions = [ f for f in glob.glob(f"{dst}/**/*.py", recursive=True) if not ("__init__" in f) ]
+extensions = [ f for f in glob.glob(f"{temp_dst}/**/*.py", recursive=True) if not ("__init__" in f) ]
     
-# Distutils
+# distutils
 setup(
-    name=src,
+    name=temp_dst,
     ext_modules=cythonize(extensions),
     cmdclass = {'build_ext': build_ext},
     build_dir=build_path
@@ -52,5 +53,8 @@ setup(
 
 # remove build and `.py`
 [ os.remove(f) for f in extensions ]
-[ os.remove(f) for f in glob.glob(f"{dst}/**/*.c", recursive=True) ]
+[ os.remove(f) for f in glob.glob(f"{temp_dst}/**/*.c", recursive=True) ]
 shutil.rmtree(build_path)
+
+# move temp_dst to dst
+shutil.move(temp_dst, dst)
